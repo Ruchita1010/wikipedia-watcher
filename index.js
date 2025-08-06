dab23dfd04023d345d9c0a0b91504efc8ede6c78
Ruchita Gosavi <70577616+Ruchita1010@users.noreply.github.com>
2025-08-07 02:21:07 +0530

    feat: display edits every minute

--- index.js
+++ index.js
@@ -1,17 +1,80 @@
 import { EventSource } from 'eventsource';
 
 const STREAM_URL = 'https://stream.wikimedia.org/v2/stream/revision-create';
-const INTERVAL = 10 * 1000;
-const evtSource = new EventSource(STREAM_URL);
+const REPORT_INTERVAL = 10 * 1000;
+const TIME_WINDOW = 5 * 60 * 1000;
 
-evtSource.addEventListener('open', () => {
+const eventSource = new EventSource(STREAM_URL);
+const eventBuffer = [];
+
+eventSource.addEventListener('open', () => {
   console.info('The connection has been established.');
 });
 
-evtSource.addEventListener('error', () => {
+eventSource.addEventListener('error', () => {
   console.info('An error occured while attempting to connect.');
 });
 
-evtSource.addEventListener('message', (e) => {
-  console.log(JSON.parse(e.data));
+eventSource.addEventListener('message', (e) => {
+  const {
+    meta: { domain },
+    page_title,
+    performer: { user_text, user_is_bot, user_edit_count },
+  } = JSON.parse(e.data);
+
+  eventBuffer.push({
+    timestamp: Date.now(),
+    domain,
+    page_title,
+    user_text,
+    user_is_bot,
+    user_edit_count,
+  });
 });
+
+setInterval(() => {
+  const domains = {};
+  const users = {};
+
+  const now = Date.now();
+  while (eventBuffer.length && eventBuffer[0].timestamp < now - TIME_WINDOW) {
+    eventBuffer.shift();
+  }
+
+  for (const event of eventBuffer) {
+    const { domain, page_title, user_text, user_is_bot, user_edit_count } =
+      event;
+
+    if (!domains[domain]) {
+      domains[domain] = new Set();
+    }
+    domains[domain].add(page_title);
+
+    if (
+      !user_is_bot &&
+      domain === 'en.wikipedia.org' &&
+      user_edit_count !== undefined
+    ) {
+      users[user_text] = user_edit_count;
+    }
+  }
+
+  const domainEntries = Object.entries(domains)
+    .map(([domain, pagesSet]) => [domain, pagesSet.size])
+    .sort((a, b) => b[1] - a[1]);
+
+  console.log(
+    `\nTotal number of Wikipedia Domains updates: ${domainEntries.length}\n`
+  );
+  domainEntries.forEach(([domain, uniquePages]) => {
+    console.log(`${domain}: ${uniquePages} updated`);
+  });
+
+  const sortedUsers = Object.entries(users).sort((a, b) => b[1] - a[1]);
+  console.log('\nUsers who made changes to en.wikipedia.org\n');
+  sortedUsers.forEach(([username, editCount]) => {
+    console.log(`${username}: ${editCount}`);
+  });
+
+  console.log('\n--------------------------------------------------\n');
+}, REPORT_INTERVAL);
